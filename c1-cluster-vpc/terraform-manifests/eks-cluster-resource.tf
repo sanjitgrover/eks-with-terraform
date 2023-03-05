6c5dd7cd0833d2bf251eea52fe4b707f86e1a783
EC2 Default User <ec2-user@ip-172-31-19-200.ec2.internal>
2023-03-05 13:32:23 +0000

    EKS IAM

---
+++ c1-cluster-vpc/terraform-manifests/eks-cluster-resource.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = local.eks_cluster_name
+  role_arn = aws_iam_role.eks_master_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
