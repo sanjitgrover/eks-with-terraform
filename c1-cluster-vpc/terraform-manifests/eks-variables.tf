ba0eb57598f69183fc8ce16dfebdf97a4f1216ab
EC2 Default User <ec2-user@ip-172-31-69-86.ec2.internal>
2023-03-05 16:55:22 +0000

    eks setup

--- c1-cluster-vpc/terraform-manifests/eks-variables.tf
+++ c1-cluster-vpc/terraform-manifests/eks-variables.tf
@@ -2,3 +2,28 @@ variable "cluster_name" {
     type = string
     default = "eksdemo"
 }
+
+variable "cluster_service_ipv4_cidr" {
+    type = string
+    default = null
+}
+
+variable "cluster_version" {
+    type = string
+    default = null
+}
+
+variable "cluster_endpoint_private_access" {
+    type = bool
+    default = false
+}
+
+variable "cluster_endpoint_public_access" {
+    type = bool
+    default = true
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
